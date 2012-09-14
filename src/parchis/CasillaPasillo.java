1d024766a27ddc96e62037be8cae35abb018ad97
Maria <Maria@maria.com>
2012-09-14 18:10:47 +0200

    hh

--- src/parchis/CasillaPasillo.java
+++ src/parchis/CasillaPasillo.java
@@ -1,14 +1,15 @@
 package parchis;
 
-
+/**
+ * Y esta tampoco
+ * @author Maite
+ *
+ */
 public class CasillaPasillo extends Casilla {
 
 	public CasillaPasillo(int numero) {
 		super(numero,true);
 		
-		
 	}
-	/**blablabla**/
-	
 
 }
